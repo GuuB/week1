b453944e30c6452d976362b9d72054e48a145191
Guy Booth <yugbooth@hotmail.com>
2016-10-06 16:26:42 +0000

    adding pythagore.js

---
+++ day4/pythagore.js
@@ -0,0 +1,13 @@
+const input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map( function(current)
+{
+  return Math.sqrt(Math.pow(current['x'], 2) + Math.pow(current['y'], 2));
+});
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
